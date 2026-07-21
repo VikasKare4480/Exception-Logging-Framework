9bcc3e5c6cf1ae4ad34ac318bb24d34cd9b11820
VikasKare <karevikas7777@gmail.com>
2026-07-21 14:56:00 +0530

    push to master

--- force-app/main/default/lwc/showToggeleLWC/showToggeleLWC.js
+++ force-app/main/default/lwc/showToggeleLWC/showToggeleLWC.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class ShowToggeleLWC extends LightningElement {}
+export default class ShowToggeleLWC extends LightningElement {
+
+        
+}
